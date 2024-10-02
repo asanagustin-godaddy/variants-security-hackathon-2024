b7d1867537fdccb71d11ed15c615647a0d2517dc
asanagustin-godaddy <asanagustin@godaddy.com>
2024-10-02 11:00:30 -0700

    add basic office map

---
+++ main/worlds/maps/objects-32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="objects-32" tilewidth="32" tileheight="32" tilecount="66" columns="11">
+ <image source="objects-32.png" width="352" height="192"/>
+</tileset>
