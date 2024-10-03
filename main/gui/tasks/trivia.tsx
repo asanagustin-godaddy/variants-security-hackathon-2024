45aa1102c99bebf0ff6a2c4e979d87187ad34cc8
Ryan Norton <rxnorton@godaddy.com>
2024-10-03 14:58:57 -0500

    update trivia

--- main/gui/tasks/trivia.tsx
+++ main/gui/tasks/trivia.tsx
@@ -1,31 +1,94 @@
 import { RpgReactContext, useEventPropagator } from '@rpgjs/client/react'
 import { useContext, useState } from 'react'
 
+const triviaQuestions = [
+    {
+        question: 'Which of the following is not one of the top 10 vulnerabilities discussed in the tech talk?',
+        answers: [
+            'SQL Injection',
+            'Cross-site scripting',
+            'Server side request forgery',
+            'Gatekeeper'
+        ],
+        correctAnswerIndex: 3
+    },
+    {
+        question: 'Who does GoDaddy partner with to discover vulnerabilities?',
+        answers: [
+            'Hackerone',
+            'Anonymous',
+            'Cyberpunk',
+            'LimeWire'
+        ],
+        correctAnswerIndex: 0
+    },
+    {
+        question: 'An access control vulnerability that arises when an application uses user-supplied input to access objects directly is known as IDOR.  What does that stand for?',
+        answers: [
+            'I don’t own raisins',
+            'Identity disabled object reference',
+            'Incapacitated Data object reference',
+            'Insecure direct object reference'
+        ],
+        correctAnswerIndex: 3
+    }
+];
+
 export default function trivia() {
     const { rpgGuiClose } = useContext(RpgReactContext)
 
     const [showModal, setShowModal] = useState(true);
-    const [answer, setAnswer] = useState('');
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [isAnswerCorrect, setIsAnswerCorrect] = useState(false);
+    const [completed, setCompleted] = useState(false);
+    const [showFeedback, setShowFeedback] = useState(false);
+
+    const handleAnswer = (answer: number) => {
+        if (answer === triviaQuestions[currentQuestion].correctAnswerIndex) {
+            setIsAnswerCorrect(true);
+            if (currentQuestion === triviaQuestions.length - 1) {
+                console.log('completed');
+                setCompleted(true);
+            }
+        } else {
+            setIsAnswerCorrect(false);
+        }
+        setShowFeedback(true);
+    }
+
+    const handleNext = () => {
+        setCurrentQuestion(currentQuestion + 1);
+        setShowFeedback(false);
+        setIsAnswerCorrect(false);
+    }
 
     if (!showModal) return null;
 
     return (
         <>
+            {completed &&
+                <div className="fixed inset-0 flex items-center justify-center z-50">
+                    <div className="flex flex-col gap-8 bg-white opacity-100 text-black p-8 rounded-lg"> 
+                        <div className="text-black">Great work, you've completed a task!</div>
+                        <button onClick={()=>setShowModal(false)} className="bg-gray-600 text-white rounded p-2 w-full">Close</button>
+                    </div>
+                </div>
+            }
             <div className="fixed inset-0 flex items-center justify-center z-50">
                 <div className="bg-white opacity-100 text-black p-4 rounded-lg">
                     <div className="flex flex-col gap-5 max-w-96 justify-between">
                         <h1 className="font-bold">It's trivia time!</h1>
-                        <p>Question here...</p>
+                        <p>{triviaQuestions[currentQuestion].question}</p>
                         <div className="grid grid-cols-2 gap-2">
-                            <button className="bg-green-500 text-white p-2 rounded">Answer 1</button>
-                            <button className="bg-red-500 text-white p-2 rounded">Answer 2</button>
-                            <button className="bg-blue-500 text-white p-2 rounded">Answer 3</button>
-                            <button className="bg-yellow-500 text-white p-2 rounded">Answer 4</button>
+                            {triviaQuestions[currentQuestion].answers.map((option, index) => (
+                                <button key={index} onClick={() => handleAnswer(index)} className="bg-teal-500 hover:opacity-90 text-white p-2 w-full rounded">{option}</button>
+                            ))}
                         </div>
                         {/* TODO: add state to answer feedback */}
-                        <div className="text-red-700">Oops! Try again.</div>
-                        <div className="text-green-700">Correct! This is because...</div>
-                        <button onClick={() => rpgGuiClose('trivia')} className="bg-gray-600 text-white rounded p-2">Close</button>
+                        {showFeedback && !isAnswerCorrect && <div className="text-red-700">Oops! Try again.</div>}
+                        {showFeedback && isAnswerCorrect && <div className="text-green-700">Correct!</div>}
+                        {!completed && <button disabled={!isAnswerCorrect} onClick={handleNext} className="disabled:bg-gray-400 bg-gray-600 text-white p-2 w-full rounded ">Next</button>}
+                        {completed && <><p>You are done!</p><button onClick={() => rpgGuiClose('trivia')} className="bg-gray-600 text-white rounded p-2">Close</button></>}
                     </div>
                 </div>
             </div>
